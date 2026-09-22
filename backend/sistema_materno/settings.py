224f3a23603b26976ca677dfe290099c4936ee5d
Axel  Plata <axelplata@MacBook-Air-de-Axel.local>
2026-09-21 22:14:20 -0300

    fix: agrega dominio fijo de Vercel a CORS_ALLOWED_ORIGINS

--- backend/sistema_materno/settings.py
+++ backend/sistema_materno/settings.py
@@ -110,6 +110,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://maternidad.vercel.app",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
